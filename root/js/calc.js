03806b54ed5564a84bd0cf00cb8dbf236834edc6
ababaSigrun <ababa715git@gmail.com>
2021-08-05 04:42:37 +0000

    MSの速度計算に必要な項目をclassにした

---
+++ root/js/calc.js
@@ -0,0 +1,13 @@
+class MobileSuit {
+    constructor(type, rare, cost, size, skill_1, skill_2, skill_3, terrainsuitability) {
+        this.type = type;
+        this.rare = rare;
+        this.cost = cost;
+        this.size = size;
+        this.skill_1 = skill_1;
+        this.skill_2 = skill_2;
+        this.skill_3 = skill_3;
+        this.terrainsuitability = terrainsuitability;
+
+    }
+}
